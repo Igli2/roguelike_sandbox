c0283aebc1720af86bd953ebbf5adafa98e5d552
Igli2 <sg.gerdon@gmx.de>
2021-10-14 13:29:20 +0200

    initial commit

---
+++ desktop/src/com/roguelike_sandbox/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.roguelike_sandbox.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.roguelike_sandbox.game.RoguelikeSandbox;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RoguelikeSandbox(), config);
+	}
+}
