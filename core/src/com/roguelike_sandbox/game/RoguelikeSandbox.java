964650e48ee52ed02a811a77a83ce5b135b1e2c1
OfficialRey <rey.official@outlook.de>
2021-10-14 15:41:44 +0200

    updated gradle gitignore

--- core/src/com/roguelike_sandbox/game/RoguelikeSandbox.java
+++ core/src/com/roguelike_sandbox/game/RoguelikeSandbox.java
@@ -8,31 +8,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class RoguelikeSandbox extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 
+    SpriteBatch batch;
+    Texture img;
 
-	//test123
+    @Override
+    public void create() {
+    }
 
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+        batch.begin();
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
+        batch.end();
+    }
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-		batch.begin();
-		// batch.draw(img, 0, 0);
-		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
--- desktop/src/com/roguelike_sandbox/game/desktop/DesktopLauncher.java
+++ desktop/src/com/roguelike_sandbox/game/desktop/DesktopLauncher.java
@@ -4,16 +4,17 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.roguelike_sandbox.game.RoguelikeSandbox;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		// config.fullscreen = true;
-		config.width = 800;
-		config.height = 600;
-		config.title = "Roguelike Sandbox";
-		config.forceExit = true;
+import java.awt.*;
 
-		new LwjglApplication(new RoguelikeSandbox(), config);
-		System.out.println("Hello World!");
-	}
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.fullscreen = true;
+        config.height = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        config.width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        config.title = "Roguelike Sandbox";
+        config.forceExit = true;
+
+        new LwjglApplication(new RoguelikeSandbox(), config);
+    }
 }
