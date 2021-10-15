cdc940900221d8e27c976c804a0040a76b717d88
Igli2 <sg.gerdon@gmx.de>
2021-10-15 15:40:52 +0200

    added new assets

--- core/assets/tilemaps/ground.tsx
+++ core/assets/tilemaps/ground.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="ground" tilewidth="32" tileheight="32" tilecount="15" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="ground" tilewidth="32" tileheight="32" tilecount="36" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../images/world/dirt.png"/>
@@ -45,5 +45,68 @@
  </tile>
  <tile id="6">
   <image width="32" height="32" source="../images/world/stone.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../images/world/path_1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../images/world/path_2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../images/world/path_3.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../images/world/path_4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../images/world/path_dirt_1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../images/world/path_dirt_2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../images/world/path_dirt_3.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="../images/world/path_dirt_4.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../images/world/path_dirt_5.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="../images/world/path_dirt_6.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="../images/world/path_dirt_7.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="../images/world/path_dirt_8.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="../images/world/path_dirt_9.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="../images/world/path_dirt_10.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="../images/world/path_dirt_11.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="../images/world/path_dirt_12.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="../images/world/path_dirt_13.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="../images/world/path_dirt_14.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="../images/world/path_dirt_15.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="../images/world/path_dirt_16.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="32" source="../images/world/path_dirt_17.png"/>
  </tile>
 </tileset>
--- core/assets/tilemaps/overlay.tsx
+++ core/assets/tilemaps/overlay.tsx
@@ -1,7 +1,58 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="overlay" tilewidth="224" tileheight="135" tilecount="1" columns="0">
+<tileset version="1.5" tiledversion="1.7.2" name="overlay" tilewidth="224" tileheight="135" tilecount="18" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="224" height="135" source="../images/world/building.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../images/world/well.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../images/world/fence_1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../images/world/fence_2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../images/world/fence_3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../images/world/fence_4.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../images/world/fence_5.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../images/world/fence_6.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../images/world/fence_7.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../images/world/fence_8.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../images/world/fence_9.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../images/world/fence_10.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../images/world/fence_11.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../images/world/fence_12.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../images/world/fence_13.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../images/world/fence_14.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="../images/world/fence_15.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../images/world/fence_16.png"/>
  </tile>
 </tileset>
